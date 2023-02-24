0955e942d1a60f28a3006bf4ff4cd5b89f24f5a3
AmarDange <amardange218@gmail.com>
2023-02-24 18:43:17 +0000

    Add cart Box Content in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,35 +40,55 @@ function removeCartItem(event){
     buttonClicked.parentElement.remove();
     updatetotal();
 };
-
-function addCartClicked(event){
-    var button = event.target
-    var shopProducts = button.parentElement
-    var title = shopProducts.getElementsByClassName('product-title')[0].innerText;
-    var price = shopProducts.getElementsByClassName('price')[0].innerText;
-    var productImg = shopProducts.getElementsByClassName('product-img')[0].src;
-    addProductToCart(title, price, productImg);
-    updatetotal();
-}
-
-function addProductToCart(title, price, productImg){
-    var cartShopBox = document.createElement("div");
-
-    var cartItems = document.getElementsByClassName('cart-content')[0]
-    var cartItemsNames = cartItems.getElementsByClassName('cart-product-title')
-    for (var i = 0; i < cartItemsNames.length; i++){
-        alert("You have already add this item to cart");
-    }
-}
-
-
-
 function quantityChanged(event){
     var input = event.target
     if(isNaN(input.value) || input.value <= 0){
         input.value = 1;
     }
     updatetotal();
+}
+
+function addCartClicked(event){
+    var button = event.target
+    var shopProducts = button.parentElement
+    var title = shopProducts.getElementsByClassName("product-title")[0].innerText;
+    var price = shopProducts.getElementsByClassName("price")[0].innerText;
+    var productImg = shopProducts.getElementsByClassName("product-img")[0].src;
+    addProductToCart(title, price, productImg);
+    updatetotal();
+}
+
+function addProductToCart(title, price, productImg) {
+    var cartShopBox = document.createElement("div");
+        cartShopBox.classList.add("cart-box");
+
+    var cartItems = document.getElementsByClassName("cart-content")[0]
+    var cartItemsNames = cartItems.getElementsByClassName("cart-product-title")
+    for (var i = 0; i < cartItemsNames.length; i++) {
+        if(cartItemsNames[i].innerText == title) {
+
+        alert("You have already add this item to cart");
+        return;
+    }
+}
+
+var cartBoxContent = `
+                        <img src= "${productImg}"  alt="" class="cart-img">
+                        <div class ="detail-box">
+                            <div class="cart-product-title">${title}</div>
+                            <div class="cart-price">${price}</div>
+                            <input type="number" value="1" class="cart-quantity">
+                        </div>
+                        <i class="bx bxs-trash-alt cart-remove" ></i>`;
+cartShopBox.innerHTML = cartBoxContent;
+cartItems.append(cartShopBox);
+cartShopBox
+    .getElementsByClassName("cart-remove")[0]
+    .addEventListener("click", removeCartItem);
+cartShopBox
+    .getElementsByClassName("cart-quantity")[0]
+    .addEventListener("change", quantityChanged);
+
 }
 
 function updatetotal(){
